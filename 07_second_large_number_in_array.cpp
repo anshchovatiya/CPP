53dd27a04d355ab77a209ddece033bcca413a945
anshchovatiya4444 <anshchovtiya800@gmail.com>
2024-02-02 18:45:10 +0530

    string explanation

--- 07_second_large_number_in_array.cpp
+++ 07_second_large_number_in_array.cpp
@@ -1,37 +1,37 @@
-// // //find secod smallest elements in array
+//find secod smallest elements in array
 
-// #include <iostream>
+#include <iostream>
 
-// using namespace std;
+using namespace std;
 
-// int main(void)
-// {
-//     int n[] = {38, 583, 8234, 68, 384, 582};
+int main(void)
+{
+    int n[] = {38, 583, 8234, 68, 384, 582};
 
-//     int count = sizeof(n) / sizeof(int);
+    int count = sizeof(n) / sizeof(int);
 
-//     int max;
+    int max;
 
-//     for (int i = 0; i < count; i++)
-//     {
-//         if (n[i] < max)
-//         {
-//             max = n[i];
-//         }
-//     }
+    for (int i = 0; i < count; i++)
+    {
+        if (n[i] < max)
+        {
+            max = n[i];
+        }
+    }
 
-//     int max2 ;
+    int max2 ;
 
-//     for (int i = 0; i < count; i++)
-//     {
-//         if (n[i] < max2)
-//         {
-//             if (n[i] != max)
-//                 max2 = n[i];
-//         }
-//     }
-//     cout << max2;
-// }
+    for (int i = 0; i < count; i++)
+    {
+        if (n[i] < max2)
+        {
+            if (n[i] != max)
+                max2 = n[i];
+        }
+    }
+    cout << max2;
+}
 
 // // write a program to copy contents of an array in another array in reverse order
 
@@ -122,32 +122,32 @@
 
 // }
 
-//  write a program to add two matrix
+// //  write a program to add two matrix
 
-#include <iostream>
+// #include <iostream>
 
-using namespace std;
+// using namespace std;
 
-int main(void)
-{
-    int n[2][2] = {{1, 2}, {4, 5}};
-    int y[2][2] = {{7, 8}, {10, 11}};
+// int main(void)
+// {
+//     int n[2][2] = {{1, 2}, {4, 5}};
+//     int y[2][2] = {{7, 8}, {10, 11}};
 
-    int sum[2][2];
+//     int sum[2][2];
 
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < 2; j++)
-        {
-            sum[i][j] = n[i][j] + y[i][j];
-        }
-    }
+//     for (int i = 0; i < 2; i++)
+//     {
+//         for (int j = 0; j < 2; j++)
+//         {
+//             sum[i][j] = n[i][j] + y[i][j];
+//         }
+//     }
 
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < 2; j++)
-        {
-            cout << sum[i][j] << endl;
-        }
-    }
-}
+//     for (int i = 0; i < 2; i++)
+//     {
+//         for (int j = 0; j < 2; j++)
+//         {
+//             cout << sum[i][j] << endl;
+//         }
+//     }
+// }
