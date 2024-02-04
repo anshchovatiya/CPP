f4f5e9b49bab5d40ed42f99e73d48ee825c7c845
anshchovatiya4444 <anshchovtiya800@gmail.com>
2024-02-04 16:09:41 +0530

    structures

--- 06_structure_pointer.cpp
+++ 06_structure_pointer.cpp
@@ -1,3 +1,5 @@
+// structure pointer 
+
 #include <iostream>
 
 using namespace std;
@@ -12,10 +14,10 @@ int main(void)
 {
 
     student ansh;
-    student *ptr = &ansh;
+    student *ptr = &ansh;  // It is pointer that points to the structure
 
     cout << "Enter name : ";
-    getline(cin, ptr->name);
+    getline(cin, ptr->name); // -> It is an arrow operator in c
 
     cout << "Enter id : ";
     cin >> ptr->id;
@@ -23,3 +25,48 @@ int main(void)
     cout << "Name : " << ptr->name << endl;
     cout << "Id : " << ptr->id << endl;
 }
+
+
+
+// // structure usig function
+
+// #include <iostream>
+
+// using namespace std;
+
+// typedef struct Student
+// {
+//     string name;
+//     long long int id;
+// } student;
+
+// student get(student *ptr);
+// void print(student *ptr);
+// int main(void)
+// {
+
+//     student ansh;
+//     student *ptr = &ansh;
+
+//     get(ptr);
+//     print(ptr);
+// }
+
+// student get(student *ptr)
+// {
+//     cout << "Enter name : ";
+//     getline(cin, ptr->name);
+
+//     cout << "Enter id : ";
+//     cin >> ptr->id;
+
+//     return *ptr;
+// }
+
+// void print(student *ptr)
+// {
+//     cout << " Name : " << ptr->name << endl;
+//     cout << " ID : " << ptr->id << endl;
+
+//     return;
+// }
