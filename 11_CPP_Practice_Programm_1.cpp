22cfb8d09a6d87bcb68ae8d98bbb4f44b0849494
anshchovatiya4444 <anshchovtiya800@gmail.com>
2024-02-06 00:06:19 +0530

    practicles complete

---
+++ 11_CPP_Practice_Programm_1.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Student
+{
+private:
+    long int admno;
+    char sname[30];
+
+      // structure of marks
+    struct Subject
+    {
+        float marks;
+    } eng, maths, science;
+
+    float total;
+
+    // private function to calculate total marks  
+    float ctotal(float m1, float m2, float m3)
+    {
+        float total = m1 + m2 + m3;
+        return total;
+    }
+
+public:
+    void Takedata()
+    {
+        cout << endl << "Enter the admno : " ;
+        cin >> admno;
+
+         // sname will take enter as input so we need to use getchar so it can take enter as input
+        getchar(); 
+
+        cout << "Enter the name of student : " ;
+        gets(sname);
+
+
+        cout << "Enter marks of eng , maths and science : " ; 
+        cin >> eng.marks >> maths.marks >> science.marks;
+
+         // calling privately defined function
+        total = ctotal(eng.marks,maths.marks,science.marks); 
+    }
+
+    // float total()
+    // {
+    //     float t1 = ctotal(eng.marks,maths.marks,science.marks);
+
+    //     return t1;
+    // }    // This is not neded 
+
+    void Showdata()
+    {
+
+        float v = ctotal(eng.marks, maths.marks, science.marks);
+
+        cout <<endl<< "admno       : " << admno << endl;
+        cout << "name        : " << sname << endl;
+        cout << "total marks : " << total << endl << " ";
+    }
+};
+
+int main(void)
+{
+    Student ansh;
+
+    ansh.Takedata();
+    ansh.Showdata();
+}
+
+
+
+
+
+// 1. Define a class student with the following specification
+
+// Private members of class student
+
+// admno                integer
+// sname                20 character
+// eng. math, science   float
+// total                float
+// ctotal()             a function to calculate eng + math + science with float return type.
+
+// Public member function of class student
+
+// Takedata()   Function to accept values for admno, sname, eng, science and invoke
+// ctotal()     to calculate total.
+// Showdata()   Function to display all the data members on the screen
---
+++ 13_CPP_Practice_Programm_3.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+
+using namespace std;
+
+class TEST
+{
+private:
+    
+    int Testcode, NoCandidate, CenterReqd;
+    string Description;
+
+    int CALCNTR(int a)
+    {
+        int ans = ((a / 100) + 1);
+        return ans;
+    }
+
+public:
+
+    // take the data from user as input and calculate other data
+    void SCHEDULE()
+    {
+        cout << endl
+             << "code of the test : ";
+        cin >> Testcode;
+
+        getchar();
+
+        // If we want to input entire string we need to use get line function
+        cout << "Write a Description : ";
+        getline(cin, Description);
+
+        cout << "Enter the number of Canidadate : ";
+        cin >> NoCandidate;
+
+        CenterReqd = CALCNTR(NoCandidate);
+    }
+
+    // print the calculated data
+    void DISPTEST()
+    {
+        cout << endl
+             << "TestCode         : " << Testcode << endl;
+        cout << "Description      : " << Description << endl;
+        cout << "NoCandidate      : " << NoCandidate << endl;
+        cout << "Required centers : " << CenterReqd << endl << " ";
+    }
+};
+
+int main(void)
+{
+    TEST June;
+
+    June.SCHEDULE();
+    June.DISPTEST();
+}
+
+
+
+
+
+
+
+
+
+
+// 3. Define a class TEST in C++ with following description:
+
+// Private Members
+
+// TestCode               of type integer
+// Description            of type string
+// NoCandidate            of type integer
+// CenterReqd             (number of centers required) of type integer
+
+// A member function CALCNTR() to calculate and return the number of centers as
+// (NoCandidates/100+1)
+
+// Public Members
+
+// - A function SCHEDULE() to allow user to enter values for TestCode, Description,
+// NoCandidate & call function CALCNTR() to calculate the number of Centres
+
+// - A function DISPTEST() to allow user to view the content of all the data members
