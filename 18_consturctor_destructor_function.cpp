751c8d349584db8a8d02597d30ba491ec7334a51
anshchovatiya4444 <anshchovtiya800@gmail.com>
2024-02-08 16:17:10 +0530

    constructor and desructor

---
+++ 18_consturctor_destructor_function.cpp
@@ -0,0 +1,58 @@
+
+// constuctor is basically building block of a class
+// when we creat object , construtor function will be automatically executed
+
+#include <iostream>
+
+using namespace std;
+
+class Student
+{
+private:
+    string s;
+
+public:
+    Student(void); // --> constructor function declaration  // construcotor function name should be same as class
+                   // --> It declars that we are consruting Student class
+
+    ~Student(); // --> destructor function decalration   // destructor function name should be same as class
+
+    void printname()
+    {
+        cout
+            << " your name is " << s << endl;
+    }
+};
+
+Student::Student(void) //  -->consturctor defination
+
+{
+    // It will automatically execute when we crate new object
+    cout << "enter the name : ";
+    cin >> s;
+
+    printname();
+}
+
+// Student::Student(void) // consturctor defination  //--> It is default constructor becuause it will not take value in output
+// {
+//     s = "ansh chovatiya";
+// }
+
+Student::~Student() // destructor defination
+{
+    // It will automatically execute when complier is destroying the object
+    cout << endl
+         << "this objects is now destroyed";
+}
+
+int main(void)
+{
+    {
+        Student ansh;
+    }
+
+    // now construction function will be automatically executed
+
+    // ansh.printname();
+}
---
+++ 19_passing_object_to_object.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+using namespace std;
+
+class Name
+{
+private:
+    string name;
+    int marks[3];
+    int total;
+    float percentage;
+
+public:
+    // declaration of all function
+    Name(void);
+    void set_data_by_sum(Name n1, Name n2);
+    void print_data();
+};
+
+int main(void)
+{
+    Name ansh, nayan, total;
+
+    ansh.print_data();
+    nayan.print_data();
+
+    total.set_data_by_sum(ansh, nayan);
+
+    total.print_data();
+}
+
+// this is constuctor function that is automatically awekand when ovjects were created
+Name ::Name(void)
+{
+    total = 0;
+
+    cout << endl
+         << "Enter your name : ";
+    getline(cin, name);
+    for (int i = 0; i < 3; i++)
+    {
+        cout << "Enter marks of sub " << i + 1 << " : ";
+        cin >> marks[i];
+        total += marks[i];
+    }
+
+    percentage = (float)total / 3.0;
+    cin.ignore();
+}
+
+// this function will set sum of to class to selected class
+void Name::set_data_by_sum(Name n1, Name n2)
+{
+    name = n1.name + " " + n2.name;
+    marks[0] = n1.marks[0] + n2.marks[0];
+    marks[1] = n1.marks[1] + n2.marks[1];
+    marks[2] = n1.marks[2] + n2.marks[2];
+    total = n1.total + n2.total;
+    percentage = n1.percentage + n2.percentage;
+}
+
+// this function will print all collected data
+void Name::print_data()
+{
+    cout << endl
+         << "Name        : " << name << endl;
+    cout << "Total marks : " << total << endl;
+    cout << "Percentage  : " << percentage << endl
+         << " ";
+}
