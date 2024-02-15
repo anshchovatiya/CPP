b225568058f3be2c43c5f00346e94a73be332b57
anshchovatiya4444 <anshchovtiya800@gmail.com>
2024-02-15 22:13:42 +0530

    multiple Inheritance

--- 34_multilevel_inheritance.cpp
+++ 34_multilevel_inheritance.cpp
@@ -15,7 +15,7 @@ public:
 };
 
 // this is inherited calss Exam whoms base class is Student
-class Exam : public Student
+class Exam : protected Student
 {
 protected:
     float Maths, Science, Chemistry;
@@ -28,24 +28,25 @@ public:
 // Exam is base class for result
 
 // this is inherited class Result whoms base class is Exam
-class Result : public Exam
+class Result : protected Exam
 {
 public:
     float total;
     float percentage;
     void calculate();
     void Display();
+    void data_marks(string,int,float,float,float);
 };
 
-// Stundent --> Exam --> Result
+// Stundent --> Exam --> Result  --> It is inheritance path
+
 
 
 
 int main(void)
 {
     Result ansh;
-    ansh.get_data("Ansh Chovatiya", 5);
-    ansh.set_marks(97.69, 95.79, 80.89);
+    ansh.data_marks("ansh chovatiya",3,90.96,89.79,89.80);
     ansh.calculate();
     ansh.Display();
 }
@@ -59,8 +60,8 @@ void Student ::get_data(string v, int b)
 void Student ::print_data()
 {
     cout << endl
-         << "Name       : " << name << endl;
-    cout << "Student Id : " << Student_Id << endl;
+         << "Name            : " << name << endl;
+    cout << "Student Id      : " << Student_Id << endl;
 }
 
 void Exam ::set_marks(float f1, float f2, float f3)
@@ -88,7 +89,14 @@ void Result ::Display()
     print_data();
     print_marks();
 
-    cout << "total marks : " << total << endl;
-    cout << "Percentage  : " << percentage << endl
+    cout << "Total marks     : " << total << endl;
+    cout << "Percentage      : " << percentage << endl
          << " ";
+}
+
+// this function will give values to other classes function becuase they are protected
+void Result:: data_marks(string a,int b,float c,float d,float e)
+{
+    get_data(a, b);
+    set_marks(c,d, e); 
 }
