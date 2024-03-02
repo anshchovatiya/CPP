b35294e54b150f818b8929afae0d31dc1c731117
anshchovatiya4444 <anshchovatiya800@gmail.com>
2024-03-02 11:36:43 +0530

    old

--- 62_Hotel_management.cpp
+++ 62_Hotel_management.cpp
@@ -1,141 +1,281 @@
 #include <iostream>
+#include <cstring>
+#include <iomanip>
 
 using namespace std;
 
 void print_menu();
 
-struct Dish
+struct Answer
 {
-    string name;
-    double normal_price;
-    double Cheese_butter_price;
-    int quantity;
+     string dish_name = "";
+     double dish_price;
+     int qty;
+     double total;
 };
 
-class Table
+class Item
 {
-    int Table_number;
-    string customer_name;
-    static int table_number;
+     string Item_name = "";
+     double Item_price;
 
 public:
+     Item(){};
+
+     Item(string name, double price)
+     {
+          Item_name = name;
+          Item_price = price;
+     }
+
+     Answer Calculator(int qty)
+     {
+          Answer result;
+
+          result.dish_name = Item_name;
+
+          result.dish_price = Item_price;
+          result.qty = qty;
+          result.total = Item_price * qty;
+
+          return result;
+     }
+
+     void print_Item_data()
+     {
+          cout << endl
+               << "Item Name : " << Item_name << endl;
+          cout << "Item Price : " << Item_price;
+     }
 };
 
 int main()
 {
 
-    cout << endl
-         << "---------------------------------------------------------------" << endl
-         << " ";
-    cout << endl
-         << "Welcome to over restaurant" << endl
-         << " ";
+     Item item[26];
 
-    cout << endl
-         << "---------------------------------------------------------------" << endl
-         << " ";
-    cout << endl
-         << "Choose menu press 1 " << endl;
-    cout << endl
-         << "Check status of table press 2" << endl;
-    cout << endl
-         << "Check out from restaurant press 3" << endl;
-    cout << endl
-         << "Make reseravations press 4" << endl;
-    cout << endl
-         << "Make a bill press 5" << endl;
-    cout << endl
-         << "Exit press 0" << endl;
+     item[1] = Item("Panner Pakoda", 220);
+     item[2] = Item("Palak Paneer ", 170);
+     item[3] = Item("Veg Burger ", 110);
+     item[4] = Item("Veg Grill Sandwich", 160);
+     item[5] = Item("Veg Fry rice", 160);
+     item[6] = Item("Veg Biryani", 350);
+     item[7] = Item("Mix Veg Pizzah", 450);
+     item[8] = Item("Jeera Rice", 160);
+     item[9] = Item("Potato Cheeps", 80);
+     item[10] = Item("Masala Dosa", 90);
+     item[11] = Item("Momos:", 90);
+     item[12] = Item("Franki", 140);
+     item[13] = Item("Sweet Corn", 110);
+     item[14] = Item("Gulab Jamun", 60);
+     item[15] = Item("Kulfi", 40);
+     item[16] = Item("Vanila Ice-Cream", 70);
+     item[17] = Item("Chocolate Ice_Cream", 110);
+     item[18] = Item("Chocolate cake", 500);
+     item[19] = Item("Thums-Up", 35);
+     item[20] = Item("Mountain Duo", 30);
+     item[21] = Item("Maza", 35);
+     item[22] = Item("Sosayo", 25);
+     item[23] = Item("Milk", 50);
+     item[24] = Item("Chocolate Milk", 80);
+     item[25] = Item("Water", 30);
 
-    cout << endl
-         << "---------------------------------------------------------------" << endl
-         << " ";
-    int number;
-    cout << endl
-         << "Enter the number : ";
-    cin >> number;
+     int Choise;
+     int count = 0;
 
-    if (number < 0 || number > 5)
-    {
-        cout << endl
-             << "Invalid number" << endl;
-        goto end;
-    }
+     Answer ItemNo[20];
+     do
+     {
+     start:
+          cout << endl
+               << "*************************************************************************************************************************************************************************************************************************************************************"
+               << endl
+               << " ";
 
-    if (number == 1)
-    {
-        print_menu();
-    }
+          cout << endl
+               << "For Menu Press 1 " << endl;
+          cout << "Oreder Dish Press 2 " << endl;
+          cout << "current Status Press 3 " << endl;
+          cout << "Make Bill press 4 " << endl;
+          cout << "Exit Press 0 " << endl;
 
-end:
-    cout << endl
-         << "Session  is complete" << endl;
+          cout << endl
+               << "*************************************************************************************************************************************************************************************************************************************************************"
+               << endl
+               << " ";
+
+          cout << endl
+               << "Enter Your Choise : ";
+          cin >> Choise;
+
+          cout << endl
+               << "*************************************************************************************************************************************************************************************************************************************************************"
+               << endl
+               << " ";
+
+          if (Choise < 0 || Choise > 4)
+          {
+               cout << endl
+                    << "Invalid Input !" << endl;
+               goto start;
+          }
+          else if (Choise == 0)
+          {
+               goto end;
+          }
+
+          else if (Choise == 1)
+          {
+               print_menu();
+          }
+
+          else if (Choise == 2)
+          {
+
+               int Dish_Number;
+               cout << endl
+                    << "Enter Dish Number : ";
+               cin >> Dish_Number;
+
+               if (Dish_Number > 25 || Dish_Number < 1)
+               {
+                    cout << endl
+                         << "Invalid Input !" << endl;
+                    goto start;
+               }
+
+               int QTY;
+
+               cout << endl
+                    << "Enter Qty : ";
+               cin >> QTY;
+
+               if (QTY < 1)
+               {
+                    cout << endl
+                         << "Invalid Input !" << endl;
+                    goto start;
+               }
+
+               ItemNo[count] = item[Dish_Number].Calculator(QTY);
+               count++;
+          }
+
+          else if (Choise == 3)
+          {
+          cout << endl
+                    << "NO           Name             qty             price             Amount  " << endl
+                    << " ";
+
+
+               for (int i = 0; i < count; i++)
+               {
+                   cout << endl
+                         << i + 1 << "  "
+                         << setw(18) << ItemNo[i].dish_name << "        " << setw(3) << ItemNo[i].qty << "             " << setw(5) << ItemNo[i].dish_price << "           " << setw(8) << ItemNo[i].total;
+               }
+
+               cout << " " << endl;
+          }
+
+          else if (Choise == 4)
+          {
+               double total = 0;
+               cout << endl
+                    << "NO                 Name              qty             price             Amount  " << endl
+                    << " ";
+
+               for (int i = 0; i < count; i++)
+               {
+                    cout << endl
+                         << i + 1 << "  "
+                         << setw(25) << ItemNo[i].dish_name << "        " << setw(3) << ItemNo[i].qty << "             " << setw(5) << ItemNo[i].dish_price << "           " << setw(8) << ItemNo[i].total;
+                    total += ItemNo[i].total;
+               }
+               cout << endl << endl
+                    << "--------------------------------------------------------------------------------"
+                    << " ";
+
+               cout << endl
+                    << "                "
+                    << "  Total  "
+                    << "                                                " << total << endl;
+          }
+
+     end:
+
+          cout
+              << " ";
+
+     } while (Choise > 0 & Choise < 5);
+
+     return 0;
 }
 
 void print_menu()
 {
-    cout << endl
-         << "-----------------------------Items-----------------------------" << endl
-         << " ";
-    cout << endl
-         << "                          Main course                          " << endl
-         << " ";
+     cout << endl
+          << "-----------------------------Items-----------------------------" << endl
+          << " ";
+     cout << endl
+          << "                          Main course                          " << endl
+          << " ";
 
-    cout << endl
-         << "Number     Item                Normal       cheese/buter    " << endl
-         << endl
-         << "  1      Paneer Pakoda          220            250          " << endl
-         << "  2      Palak Paneer           170            190          " << endl
-         << "  3      Veg Burger             110            140          " << endl
-         << "  4      Veg Grill  Sandwich    160            200          " << endl
-         << "  5      Veg Fry rice           160             -           " << endl
-         << "  6      Veg Biryani            350             -           " << endl
-         << "  7      Mix Veg Pizzah         450            500          " << endl
-         << "  8      Jeera Rice             160             -           " << endl
-         << "  9      Potato Cheeps          80             110          " << endl
-         << "  10     Masala Dosa            90             130          " << endl
-         << "  11     Momos                  90             140          " << endl
-         << "  12     Franki                 140            180          " << endl
-         << "  13     Sweet Corn             110            150          " << endl
-         << " ";
+     cout << endl
+          << "Number     Item                Normal           " << endl
+          << endl
+          << "  1      Paneer Pakoda          220             " << endl
+          << "  2      Palak Paneer           170             " << endl
+          << "  3      Veg Burger             110             " << endl
+          << "  4      Veg Grill  Sandwich    160             " << endl
+          << "  5      Veg Fry rice           160             " << endl
+          << "  6      Veg Biryani            350             " << endl
+          << "  7      Mix Veg Pizzah         450             " << endl
+          << "  8      Jeera Rice             160             " << endl
+          << "  9      Potato Cheeps          80              " << endl
+          << "  10     Masala Dosa            90              " << endl
+          << "  11     Momos                  90              " << endl
+          << "  12     Franki                 140             " << endl
+          << "  13     Sweet Corn             110             " << endl
+          << " ";
 
-    cout << endl
-         << "---------------------------------------------------------------"
-         << " ";
+     cout << endl
+          << "---------------------------------------------------------------"
+          << " ";
 
-    cout << endl
-         << endl
-         << "                           Desert                           " << endl
-         << " ";
+     cout << endl
+          << endl
+          << "                           Desert                           " << endl
+          << " ";
 
-    cout << endl
-         << "Number     Item                Normal         " << endl
-         << endl
-         << "  14     Gulab Jamun            60            " << endl
-         << "  15     Kulfi                  40            " << endl
-         << "  16     Vanil Ice-Cream        70            " << endl
-         << "  17     Chocolate Ice-Cream    80            " << endl
-         << "  18     chocolate Cake         500           " << endl
-         << " ";
+     cout << endl
+          << "Number     Item                Normal         " << endl
+          << endl
+          << "  14     Gulab Jamun            60            " << endl
+          << "  15     Kulfi                  40            " << endl
+          << "  16     Vanil Ice-Cream        70            " << endl
+          << "  17     Chocolate Ice-Cream    80            " << endl
+          << "  18     chocolate Cake         500           " << endl
+          << " ";
 
-    cout << endl
-         << "---------------------------------------------------------------"
-         << " ";
+     cout << endl
+          << "---------------------------------------------------------------"
+          << " ";
 
-    cout << endl
-         << endl
-         << "                           Drinks                           " << endl
-         << " ";
+     cout << endl
+          << endl
+          << "                           Drinks                           " << endl
+          << " ";
 
-    cout << endl
-         << "Number    Item                Normal         " << endl
-         << endl
-         << "  19     Thums-Up              35            " << endl
-         << "  20     Mountain duo          30            " << endl
-         << "  21     Mazza                 35            " << endl
-         << "  22     Sosyo                 25            " << endl
-         << "  23     Mile                  50            " << endl
-         << "  24     Chocolat Milk         80            " << endl
-         << "  25     Water                 30            " << endl
-         << " ";
+     cout << endl
+          << "Number    Item                Normal         " << endl
+          << endl
+          << "  19     Thums-Up              35            " << endl
+          << "  20     Mountain duo          30            " << endl
+          << "  21     Mazza                 35            " << endl
+          << "  22     Sosyo                 25            " << endl
+          << "  23     Mile                  50            " << endl
+          << "  24     Chocolat Milk         80            " << endl
+          << "  25     Water                 30            " << endl
+          << " ";
 }
---
+++ array_passing.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+// #include <cstring>
+
+using namespace std;
+
+
+
+int main(void)
+{
+    string v;
+
+    cout << "enter the string : " ;
+    getline(cin , v);
+
+   
+
+    cout << "The string is " <<  s;
+}
